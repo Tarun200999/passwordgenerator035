e68e7a187fff1295a690db02a8416fc1aa4bbadb
TARUN <57087617+Tarun200999@users.noreply.github.com>
2022-11-04 09:22:47 +0530

    Update footer.js

--- src/component/footer.js
+++ src/component/footer.js
@@ -3,7 +3,7 @@ function Footer(){
     return (
         <div className="jumbotron">
         <h1 className="display-5">Github</h1>
-        <p className="lead"><a href="https://github.com/Tarun200999/passwordgenerator1" target="_blank">PasswordGenerator(github repo)</a></p>
+        <p className="lead"><a href="https://github.com/Tarun200999/passwordgenerator1" target="_blank">Passwor dGenerator(github repo)</a></p>
       </div>
     )
     
